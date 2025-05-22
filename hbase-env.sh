073a273e76b08cbafe3ebb5c0d141950b99eadfd
doaaredaa <doaareda1j@example.com>
2025-05-22 17:15:14 +0300

    added  hbase-env.sh

---
+++ hbase-env.sh
@@ -0,0 +1,7 @@
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export HBASE_HOME=/usr/local/hbase
+export HBASE_MANAGES_ZK=false
+export HBASE_LOG_DIR=/hadoop/hbase/logs
+export HBASE_PID_DIR=/hadoop/hbase/tmp
+export HADOOP_HOME=/usr/local/hadoop
+export HBASE_CLASSPATH=$HADOOP_HOME/etc/hadoop
